4e74a87ea996e28b518f7ba3a0d9294f7a8555c4
dgentles <dgentles@cloudfirstinc.com>
2024-12-28 21:41:51 -0500

    Update run.ps1

--- Implement-GetClientDomain/run.ps1
+++ Implement-GetClientDomain/run.ps1
@@ -1,3 +1,54 @@
+<# 
+
+.SYNOPSIS
+    
+    This function is used get the client domain for the tenant and place it into a cloud radial token
+
+.DESCRIPTION
+
+    This function gets the primary client domain 
+
+    The function requires the following environment variables to be set:
+
+    Ms365_AuthAppId - Application Id of the service principal
+    Ms365_AuthSecretId - Secret Id of the service principal
+    Ms365_TenantId - Tenant Id of the Microsoft 365 tenant
+    SecurityKey - Optional, use this as an additional step to secure the function
+
+    The function requires the following modules to be installed:
+    
+    Microsoft.Graph
+
+.INPUTS
+
+    UserEmail - user email address that exists in the tenant
+    GroupName - group name that exists in the tenant
+    TenantId - string value of the tenant id, if blank uses the environment variable Ms365_TenantId
+    TicketId - optional - string value of the ticket id used for transaction tracking
+    SecurityKey - Optional, use this as an additional step to secure the function
+
+    JSON Structure
+
+    {
+        "UserEmail": "email@address.com",
+        "GroupName": "Group Name",
+        "TenantId": "12345678-1234-1234-123456789012",
+        "TicketId": "123456,
+        "SecurityKey", "optional"
+    }
+
+.OUTPUTS
+
+    JSON response with the following fields:
+
+    Message - Descriptive string of result
+    TicketId - TicketId passed in Parameters
+    ResultCode - 200 for success, 500 for failure
+    ResultStatus - "Success" or "Failure"
+
+#>
+
+
 using namespace System.Net
 
 param($Request, $TriggerMetadata)
@@ -7,6 +58,12 @@ Write-Host "Function triggered to get default domain for tenant."
 
 # Extract the tenant ID from the request body
 $tenantId = $Request.Body.TenantId
+$SecurityKey = $env:SecurityKey
+
+if ($SecurityKey -And $SecurityKey -ne $Request.Headers.SecurityKey) {
+    Write-Host "Invalid security key"
+    break;
+}
 
 # Validate the tenant ID
 if (-not $tenantId) {
@@ -25,6 +82,7 @@ if (-not $tenantId) {
 # Define the client ID and client secret (stored in environment variables for security)
 $clientId = $env:Ms365_AuthAppId
 $clientSecret = $env:Ms365_AuthSecretId
+
 
 # Get the access token
 $body = @{
