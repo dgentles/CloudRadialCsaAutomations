93b9cab7637fcca0315c90e1be395a7afd157cd6
dgentles <dgentles@cloudfirstinc.com>
2024-12-28 20:10:17 -0500

    Update run.ps1 - Exchange Settings

--- Implement-Exchwhitelists/run.ps1
+++ Implement-Exchwhitelists/run.ps1
@@ -1,19 +1,89 @@
-# Define the parameters for the mail flow rule
+using namespace System.Net
+
+param($Request, $TriggerMetadata)
+
+Write-Host "CF Whitelist function triggered."
 $ruleName = "CF_Managed - Whitelist Cloudfirst Users"
 $recipientAddresses = @("ask@cloudfirstinc.com", "dgentles@cloudfirstinc.com", "helpdesk@cloudfirstinc.com")
 $sclValue = -1
+$resultCode = 200
+$message = ""
+$ClientDomain = $Request.Body.ClientDomain
+$TenantId = $Request.Body.TenantId
+$TicketId = $Request.Body.TicketId
+$SecurityKey = $env:SecurityKey
 
+if ($SecurityKey -And $SecurityKey -ne $Request.Headers.SecurityKey) {
+    Write-Host "Invalid security key"
+    break;
+}
+
+if (-Not $ClientDomain) {
+    $message = "Domain cannot be blank."
+    $resultCode = 500
+}
+else {
+    $ClientDomain = $GroupName.Trim()
+}
+
+if (-Not $TicketId) {
+    $TicketId = ""
+}
+
+Write-Host "Client Domain: $ClientDomain"
+Write-Host "Ticket Id: $TicketId"
+
+if ($resultCode -Eq 200) {
 # Connect to Exchange Online using managed identity
 #Connect-ExchangeOnline -ManagedIdentity -Organization "yourdomain.onmicrosoft.com"
 #Connect-ExchangeOnline -UserPrincipalName "user@domain.com"
 #Connect-ExchangeOnline -DelegatedOrganization "clientdomain.onmicrosoft.com"
-Connect-ExchangeOnline 
+#Connect-ExchangeOnline 
 
-# Create the mail flow rule
+
+# Get the access token
+$bodytk = @{
+    grant_type    = "client_credentials"
+    client_id     = $env:Ms365_AuthAppId
+    client_secret = $env:Ms365_AuthSecretId
+    scope         = "https://outlook.office365.com/.default"
+}
+
+$response = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantId/oauth2/v2.0/token" -Method Post -ContentType "application/x-www-form-urlencoded" -Body $bodytk
+$accessToken = $response.access_token
+
+# Connect to Exchange Online using the access token
+Connect-ExchangeOnline -AccessToken $accessToken -DelegatedOrganization $ClientDomain
+}
+
+
+# Check if the transport rule exists
+$rule = Get-TransportRule -Identity $ruleName -ErrorAction SilentlyContinue
+
+# Output the result
+if ($null -ne $rule) {
+    Write-Output "Transport rule '$ruleName' exists."
+} else {
+    Write-Output "Transport rule '$ruleName' does not exist."
+    # Create the mail flow rule
 New-TransportRule -Name $ruleName `
     -RecipientAddressContainsWords $recipientAddresses `
     -SenderIsExternal `
     -SetSCL $sclValue
+}
 
+$body = @{
+    Message      = $message
+    TicketId     = $TicketId
+    ResultCode   = $resultCode
+    ResultStatus = if ($resultCode -eq 200) { "Success" } else { "Failure" }
+} 
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+        StatusCode  = [HttpStatusCode]::OK
+        Body        = $body
+        ContentType = "application/json"
+    })
 # Disconnect from Exchange Online
 Disconnect-ExchangeOnline -Confirm:$false
