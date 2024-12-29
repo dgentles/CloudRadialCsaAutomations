c057c1a6b02a8442e0e4e03fc74c6f3c37a3bc30
dgentles <dgentles@cloudfirstinc.com>
2024-12-28 23:31:17 -0500

    Update run.ps1

--- Implement-GetClientDomain/run.ps1
+++ Implement-GetClientDomain/run.ps1
@@ -2,52 +2,47 @@
 
 .SYNOPSIS
     
-    This function is used get the client domain for the tenant and place it into a cloud radial token
+    This function is used to get the client domain for the tenant and place it into a cloud radial token.
 
 .DESCRIPTION
 
-    This function gets the primary client domain 
+    This function gets the primary client domain.
 
     The function requires the following environment variables to be set:
-
-    Ms365_AuthAppId - Application Id of the service principal
-    Ms365_AuthSecretId - Secret Id of the service principal
-    Ms365_TenantId - Tenant Id of the Microsoft 365 tenant
-    SecurityKey - Optional, use this as an additional step to secure the function
+    - Ms365_AuthAppId: Application Id of the service principal
+    - Ms365_AuthSecretId: Secret Id of the service principal
+    - Ms365_TenantId: Tenant Id of the Microsoft 365 tenant
+    - SecurityKey: Optional, use this as an additional step to secure the function
 
     The function requires the following modules to be installed:
-    
-    Microsoft.Graph
+    - Microsoft.Graph
 
 .INPUTS
 
-    UserEmail - user email address that exists in the tenant
-    GroupName - group name that exists in the tenant
-    TenantId - string value of the tenant id, if blank uses the environment variable Ms365_TenantId
-    TicketId - optional - string value of the ticket id used for transaction tracking
-    SecurityKey - Optional, use this as an additional step to secure the function
+    - UserEmail: User email address that exists in the tenant
+    - GroupName: Group name that exists in the tenant
+    - TenantId: String value of the tenant id, if blank uses the environment variable Ms365_TenantId
+    - TicketId: Optional - string value of the ticket id used for transaction tracking
+    - SecurityKey: Optional, use this as an additional step to secure the function
 
-    JSON Structure
-
+    JSON Structure:
     {
         "UserEmail": "email@address.com",
         "GroupName": "Group Name",
         "TenantId": "12345678-1234-1234-123456789012",
-        "TicketId": "123456,
-        "SecurityKey", "optional"
+        "TicketId": "123456",
+        "SecurityKey": "optional"
     }
 
 .OUTPUTS
 
     JSON response with the following fields:
-
-    Message - Descriptive string of result
-    TicketId - TicketId passed in Parameters
-    ResultCode - 200 for success, 500 for failure
-    ResultStatus - "Success" or "Failure"
+    - Message: Descriptive string of result
+    - TicketId: TicketId passed in Parameters
+    - ResultCode: 200 for success, 500 for failure
+    - ResultStatus: "Success" or "Failure"
 
 #>
-
 
 using namespace System.Net
 
@@ -60,17 +55,17 @@ Write-Host "Function triggered to get default domain for tenant."
 $tenantId = $Request.Body.TenantId
 $SecurityKey = $env:SecurityKey
 
-if ($SecurityKey -And $SecurityKey -ne $Request.Headers.SecurityKey) {
+if ($SecurityKey -and $SecurityKey -ne $Request.Headers.SecurityKey) {
     Write-Host "Invalid security key"
-    break;
+    return
 }
 
 # Validate the tenant ID
 if (-not $tenantId) {
+    Write-Host "Function triggered, however Tenant ID is empty"
     $response = @{
         status = 400
         body = "Tenant ID is required."
-        Write-Host "Function triggered However Tenant ID is empty"
     }
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
         StatusCode  = [HttpStatusCode]::BadRequest
@@ -80,12 +75,11 @@ if (-not $tenantId) {
     return
 }
 
-Write-Host $tenantID
+Write-Host $tenantId
 
 # Define the client ID and client secret (stored in environment variables for security)
 $clientId = $env:Ms365_AuthAppId
 $clientSecret = $env:Ms365_AuthSecretId
-
 
 # Get the access token
 $body = @{
@@ -120,13 +114,14 @@ if ($ClientDomain) {
             ClientDomain = $ClientDomain.id
         }
     }
+    Write-Host "ClientDomain =" $ClientDomain.id
 } else {
     $response = @{
         status = 404
         body = "Default domain not found."
     }
 }
-Write-Host "ClientDomain =" $ClientDomain.id
+
 # Return the response
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode  = [HttpStatusCode]::OK
